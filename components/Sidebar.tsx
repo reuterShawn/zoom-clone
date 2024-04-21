6a7c2bf5517c9f39057effd97d4d4127348536c7
reuterShawn <reutershawn45@gmail.com>
2024-04-21 02:57:26 -0500

    Setup app

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,9 +2,10 @@
 
 import { sidebarLinks } from '@/constants'
 import { cn } from '@/lib/utils';
-import { Link } from 'lucide-react';
+import  Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import React from 'react'
+import Image from 'next/image'
 
 const Sidebar = () => {
 
@@ -13,22 +14,30 @@ const Sidebar = () => {
   return (
     <section className='sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]'>
         <div className='flex flex- flex-col gap-6'>
-            {sidebarLinks.map((link) => {
-                const isActive = pathname === link.route || pathname.startsWith(link.route);
+            {sidebarLinks.map((item) => {
+                const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`);
 
                 return (
                     <Link
-                        href={link.route}
-                        key={link.label}
+                        href={item.route}
+                        key={item.label}
                         className={cn('flex gap-4 items-center p-4 rounded-lg justify-start', {
                             
                          'bg-blue-1': isActive, 
                     })}
                     >
-                        {link.label}
-                    </Link>
-                );
-                  })}
+                <Image
+                src={item.imgUrl}
+                alt={item.label}
+                width={24}
+                height={24}
+              />
+              <p className="text-lg font-semibold max-lg:hidden">
+                {item.label}
+              </p>
+            </Link>
+          );
+        })}
         </div>
     </section>
             
