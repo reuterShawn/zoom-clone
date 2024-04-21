f62092aee0a3d98e8415198a0e78033495032301
reuterShawn <reutershawn45@gmail.com>
2024-04-21 03:45:01 -0500

    Create meeting modal component

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -1,14 +1,18 @@
 "use client"
 
-import Image from 'next/image'
 import React, { useState } from 'react'
 import HomeCard from './HomeCard'
-import router, { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
+import MeetingModal from './MeetingModal'
 
 const MeetingTypeList = () => {
     const router = useRouter();
     const [meetingState, setMeetingState] = useState<'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>(undefined);
-  return (
+  
+    const createMeeting = () => {
+
+    }
+    return (
     <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
         <HomeCard 
             img="/icons/add-meeting.svg"
@@ -16,7 +20,6 @@ const MeetingTypeList = () => {
             description="Start an instant meeting"
             className="bg-orange-1"
             handleClick={() => setMeetingState('isInstantMeeting')}
-
         />
         <HomeCard 
             img="/icons/schedule.svg"
@@ -24,8 +27,6 @@ const MeetingTypeList = () => {
             description="Plan your meeting"
             className="bg-blue-1"
             handleClick={() => setMeetingState('isScheduleMeeting')}
-    
-
         />
         <HomeCard 
             img="/icons/join-meeting.svg"
@@ -33,7 +34,6 @@ const MeetingTypeList = () => {
             description="via invitation link"
             className="bg-purple-1"
             handleClick={() => setMeetingState('isJoiningMeeting')}
-
         />
         <HomeCard 
             img="/icons/recordings.svg"
@@ -41,10 +41,16 @@ const MeetingTypeList = () => {
             description="Meetings Recordings"
             className="bg-yellow-1"
             handleClick={() => router.push('/recordings')}
-
-
         />
 
+        <MeetingModal
+            isOpen={meetingState === 'isInstantMeeting'}
+            onClose={() => setMeetingState(undefined)}
+            title="Start an Instant Meeting"
+            className="text-center"
+            buttonText="Start Meeting"
+            handleClick={createMeeting}
+            />
     </section>
 )
 }
